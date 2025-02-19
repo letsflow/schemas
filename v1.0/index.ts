77c05dc9b42edd1c0da925f4b62050a3adf75cdc
arnold <arnold@jasny.net>
2025-02-19 13:21:13 +0100

    Added schemas v1.0 Added _header to serve files as JSON

---
+++ v1.0/index.ts
@@ -0,0 +1,6 @@
+export { default as fnSchema } from './fn.json';
+export { default as schemaSchema } from './schema.json';
+export { default as actionSchema } from './action.json';
+export { default as actorSchema } from './actor.json';
+export { default as scenarioSchema } from './scenario.json';
+export { default as processSchema } from './process.json';
